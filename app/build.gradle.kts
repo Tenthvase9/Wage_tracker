85dfa76609e5449015dfc44099db65f0c38a0a52
tenthvase9-del <tenthvase9@gmail.com>
2026-05-04 11:08:32 +0200

    apk commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.google.code.gson:gson:2.10.1")
